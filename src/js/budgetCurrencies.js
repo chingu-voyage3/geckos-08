570bac878d04d1306440c44e4dd2f123a7863a7e
ksc23 <ksc.main@gmail.com>
2018-01-06 07:10:51 -0800

    Add budgetCurrencies.js

---
+++ src/js/budgetCurrencies.js
@@ -0,0 +1,9 @@
+// If budgetApp is undefined, assign new obj
+// else use existing budgetApp obj
+var budgetApp = budgetApp || {};
+
+budgetApp.currencies = {
+  USD : String.fromCharCode(36),
+  ALL : String.fromCharCode(76, 101, 107)
+};
+
